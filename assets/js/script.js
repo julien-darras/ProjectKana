04dbb8b14d2ead545053f50d8c4ae4a8957ff778
Julien <julien.darras12@gmail.com>
2021-08-25 15:06:24 +0200

    ajout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
+let welcome = document.getElementById('welcome')
 let btnInscription = document.getElementById('inscriptionValid')
+
+welcome.onmouseover = () => {
+    welcome.innerHTML = 'Bienvenue'
+}
+welcome.onmouseleave = () => {
+    welcome.innerHTML = 'ようこそ'
+}
+
+
+
 
 btnInscription.onclick = () => {
     alert('おめでとう　Vous êtes inscrit')
 }
+
+
+
